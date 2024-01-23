4b9a7ae61225d68bb43ae6de7be463001fe25521
jasperstiekema <j.stiekema@student.tue.nl>
2024-01-23 23:16:36 +0100

    Color and verification added

--- Visualization_Classes.py
+++ Visualization_Classes.py
@@ -12,7 +12,7 @@ import numpy as np
 import csv
 import ast
 import geopandas as gpd
-
+from sklearn.decomposition import PCA
 
 
 class VisualizationParent(GrandParent):
@@ -62,40 +62,12 @@ class VisualizationPLT(VisualizationParent):
                 axs[row+1][col].axis('off')
 
         plt.show()
-
-
-
-class VisualizationCOLOR(VisualizationParent):
-    def __init__(self) -> None:
-        self.version: float | str = '1.1 WIP'
-        self.name: str = "PLT"
-
-    def run(self, **kwargs):
-        summary = kwargs['summary']
-        images = kwargs['images']
-
-        data: temp[int, tensor] = kwargs['embeddings']
-        # Check if the number of dimensions is not larger than the number of data points
-        key = list(data.keys())[0]
-        max_dimensions = min(len(data[key]), len(data))
-        N: int = kwargs['N_dimensions'] if kwargs['N_dimensions'] < max_dimensions else max_dimensions
-        # Extract numerical values from tensors
-        numerical_data = torch.stack(list(data.values())).numpy()
-        pca = PCA(n_components=N)
-        pca.fit(numerical_data)
-        transformed_data = pca.transform(numerical_data)
-        # Create a dictionary with IDs as keys and transformed data as values for overview
-        result_dict = {id_: transformed_data[i].tolist() for i, id_ in enumerate(data.keys())}
-
-        return result_dict
-    
-
-        
+  
 
 class VisualizationPLT2(VisualizationParent):
     def __init__(self) -> None:
-        self.version: float | str = 1.2
-        self.name: str = "PLT"
+        self.version: float | str = 1.1
+        self.name: str = "PLT2"
 
     def run(self, **kwargs):
         summary = kwargs['summary']
@@ -175,6 +147,103 @@ class VisualizationPLT2(VisualizationParent):
         plt.show()
 
 
+# class VisualizationCOLOR(VisualizationParent):
+#     def __init__(self) -> None:
+#         self.version: float | str = '1.1 WIP'
+#         self.name: str = "PLT"
+
+#     def run(self, **kwargs):
+#         summary = kwargs['summary']
+#         images = kwargs['images']
+
+#         data: temp[int, tensor] = kwargs['embeddings']
+#         # Check if the number of dimensions is not larger than the number of data points
+#         key = list(data.keys())[0]
+#         max_dimensions = min(len(data[key]), len(data))
+#         N: int = kwargs['N_dimensions'] if kwargs['N_dimensions'] < max_dimensions else max_dimensions
+#         # Extract numerical values from tensors
+#         numerical_data = torch.stack(list(data.values())).numpy()
+#         pca = PCA(n_components=N)
+#         pca.fit(numerical_data)
+#         transformed_data = pca.transform(numerical_data)
+#         # Create a dictionary with IDs as keys and transformed data as values for overview
+#         result_dict = {id_: transformed_data[i].tolist() for i, id_ in enumerate(data.keys())}
+
+#         return result_dict
+    
+class VisualizationCOLOR(VisualizationParent):
+    def __init__(self) -> None:
+        self.version: float | str = '1.2 WIP'
+        self.name: str = "COLOR"
+
+    def run(self, **kwargs):
+        summary = kwargs['summary']
+        images = kwargs['images']
+        data: temp[int, tensor] = kwargs['embeddings']
+        neighbourhood_id = kwargs['neighbourhood_id']
+
+        summary_embeddings = []
+        for cluster in summary:
+            id = (summary[cluster]['selected'])
+            if id in data[str(neighbourhood_id)]:
+                summary_embeddings.append(data[str(neighbourhood_id)][id])
+            else:
+                raise ValueError('id not in neighbourhood embeddings')
+        
+        neighbourhood_embeddings = [data[str(neighbourhood_id)][key] for key in data[str(neighbourhood_id)]]
+
+        percentage = self.tensor_average_percentage_difference(neighbourhood_embeddings, summary_embeddings)
+        print(percentage)
+
+        #add some functionality to only train pca once and store it as an attribute
+        pca = self.train_pca_on_data(data)
+        result = self.transform_tuples_with_pca(pca, neighbourhood_embeddings)
+        color = np.mean(result, axis=0)
+
+        print(color)
+
+        return(color, percentage)
+        
+    def tensor_average_percentage_difference(self, group1: list[torch.Tensor], group2: list[torch.Tensor]):
+        # Check if groups are not empty
+        if not group1 or not group2:
+            raise ValueError("Input tensor groups should not be empty")
+
+        # Calculate the average tensor for each group
+        avg_tensor1 = sum(group1) / len(group1)
+        avg_tensor2 = sum(group2) / len(group2)
+        
+        # Calculate the norm of the difference and of group 1
+        norm1 = torch.norm(avg_tensor1).item()
+        norm2 = torch.norm(avg_tensor1 - avg_tensor2).item()
+
+        if norm1 != 0:
+            size = norm2/norm1
+        else:
+            size = 1
+        return size
+    
+    def train_pca_on_data(self, dict_of_dicts):
+        # Flatten the tuples into a list
+        data = [tup for subdict in dict_of_dicts.values() for tup in subdict.values()]
+
+        # Train PCA model
+        pca = PCA(n_components=3)
+        pca.fit(data)
+        return pca
+    
+    def transform_tuples_with_pca(self, pca, tuple_list):
+        # Transform the list of tuples using the trained PCA
+        transformed_data = pca.transform(tuple_list)
+        return transformed_data
+    
+    def average_of_transformed(self, dict_of_dicts, pca):
+        averages = {}
+        for key, subdict in dict_of_dicts.items():
+            transformed_data = transform_tuples_with_pca(pca, list(subdict.values()))
+            averages[key] = np.mean(transformed_data, axis=0)
+        return averages
+
 if __name__ == '__main__':
     summary = {'7': {'Cluster 0': {'selected': '52616', 'cluster': ['52616', '52617', '52620', '52621', '52624', '52625', '52628', '52629', '52630', '52631', '52632', '52634', '52635', '52268', '52269', '52270', '52271', '52272', '52273', '52274', '52276', '52278', '52280', '52281', '52282', '52284', '52285', '52286']}, 'Cluster 1': {'selected': '52277', 'cluster': ['52618', '52619', '52622', '52623', '52626', '52627', '52633', '52275', '52277', '52279', '52283', '52287']}}}
     
@@ -189,12 +258,52 @@ if __name__ == '__main__':
     image_embeddings = dict()
     image_embeddings['7'] = embeddings
 
-    Test = VisualizationPLT2()
+    Test = VisualizationCOLOR()
     Test.run(summary = summary['7'], embeddings=image_embeddings, neighbourhood_id='7', images = images)
+
+
+# def train_pca_on_data(dict_of_dicts):
+#     # Flatten the tuples into a list
+#     data = [tup for subdict in dict_of_dicts.values() for tup in subdict.values()]
+
+#     # Train PCA model
+#     pca = PCA(n_components=3)
+#     pca.fit(data)
+#     return pca
+
+# def transform_tuples_with_pca(pca, tuple_list):
+#     # Transform the list of tuples using the trained PCA
+#     transformed_data = pca.transform(tuple_list)
+#     return transformed_data
+
+# def average_of_transformed(dict_of_dicts, pca):
+#     averages = {}
+#     for key, subdict in dict_of_dicts.items():
+#         transformed_data = transform_tuples_with_pca(pca, list(subdict.values()))
+#         averages[key] = np.mean(transformed_data, axis=0)
+#     return averages
+
+# Example usage
+# dict_of_dicts = {'category1': {'item1': (1,2,3), 'item2': (4,5,6)}, 'category2': {'item3': (7,8,9)}}
+# pca_model = train_pca_on_dict(dict_of_dicts)
+# transformed_averages = average_of_transformed(dict_of_dicts, pca_model)
 
 
 # ----------------
     
+
+        # # Check if the number of dimensions is not larger than the number of data points
+    # key = list(data.keys())[0]
+    # max_dimensions = min(len(data[key]), len(data))
+    # N: int = kwargs['N_dimensions'] if kwargs['N_dimensions'] < max_dimensions else max_dimensions
+    # # Extract numerical values from tensors
+    # numerical_data = torch.stack(list(data.values())).numpy()
+    # pca = PCA(n_components=N)
+    # pca.fit(numerical_data)
+    # transformed_data = pca.transform(numerical_data)
+    # # Create a dictionary with IDs as keys and transformed data as values for overview
+    # result_dict = {id_: transformed_data[i].tolist() for i, id_ in enumerate(data.keys())}
+    #return result_dict
 
     # Create a figure
     # fig = plt.figure(figsize=(10, 10), facecolor='black')
